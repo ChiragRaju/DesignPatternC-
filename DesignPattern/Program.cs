392c6fd3209760d57888945ee53f6a1f09d3d257
Chirag Raju <chiragrajus2002@gmail.com>
2024-12-25 14:46:40 +0530

    Add project files.

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using DesignPattern;
+using System;
+
+
+class Program {
+    public static void Main(string[] args) {
+
+        var fromEmployee =  Singleton.GetInstance;
+        Singleton.PrintDetails("This is first Message");
+
+        var fromstudent = Singleton.GetInstance;
+        Singleton.PrintDetails("This is from Student");
+        Console.ReadLine();
+
+
+}
+        
+}
+
---
+++ DesignPattern/Singleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    
+    // we use sealed class because the nested class can create an intance pf object
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        private static readonly Object obj=new object();
+        private Singleton() {
+            counter++;
+
+            Console.WriteLine("Counter value" + counter.ToString());
+        }
+
+        //THIS IS NOT EFFICINET FOR MULTITHREADED so we use Lock //refer double checked locking
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    lock (obj)
+                    {
+                        if (instance == null)
+                        { 
+                           instance = new Singleton(); 
+                        }
+                    }
+
+                }
+                
+                return instance;
+            }
+        }
+
+
+
+        public static void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
